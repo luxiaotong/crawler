b0bb7675a2d0fb9090a0ec61f752ad693812fd82
luxiaotong <luxiaotong713@sina.com>
2019-07-20 17:16:59 +0800

    pyyadi

--- pyyadi/pyyadi.js
+++ pyyadi/pyyadi.js
@@ -13,13 +13,17 @@ request('https://www.yadea.com.cn/scripts/storeData.js', function (error, respon
     fs.writeFile("./data.js", js_obj, (err) => {
         console.log("Successfully Written to data.js");
         require('./data.js')
-        rows = [ { 'code':'code', 'name':'name', 'address':'address', 'tel':'tel', 'gps':'gps', 'level':'level', 'openTime':'openTime', 'isClose':'isClose'}]
+        rows = [ { 'code':'code', 'name':'name', 'address':'address', 'tel':'tel', 'gps':'gps', 'level':'level', 'openTime':'openTime', 'isClose':'isClose', 'pcode':'pcode', 'ccode':'ccode', 'dcode':'dcode'}]
         for (var k1 in storeData) {
             for (var k2 in storeData[k1]) {
                 for (var k3 in storeData[k1][k2]) {
                     for (var k4 in storeData[k1][k2][k3]) {
                         store = storeData[k1][k2][k3][k4]
+                        store['pcode'] = k1
+                        store['ccode'] = k2
+                        store['dcode'] = k3
                         rows.push(store)
+                        console.log(store)
                     }
                 }
             }
@@ -31,5 +35,3 @@ request('https://www.yadea.com.cn/scripts/storeData.js', function (error, respon
 
     }); //end of write file
 }); //end of request
-
-
